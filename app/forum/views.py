2357ec745d6ccc9bdba4fe85caa765ffa332b079
Ahmed Alsawi <ahmedalsawi2012@gmail.com>
2019-10-15 21:44:44 +0200

    Added bootstrap

--- app/forum/views.py
+++ app/forum/views.py
@@ -5,4 +5,13 @@ from flask import render_template
 
 @app.route("/", methods=["GET"])
 def index():
-    return render_template("index.jinja")
+    return render_template("home.jinja")
+
+
+@app.route("/forum", methods=["GET"])
+def forum():
+    return render_template("home.jinja")
+
+@app.route("/forum/:id", methods=["GET"])
+def thread():
+    return render_template("home.jinja")
