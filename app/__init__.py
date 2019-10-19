6d2c03f27629513695b8665cc1c2bc5781851417
Ahmed Alsawi <ahmedalsawi2012@gmail.com>
2019-10-19 09:33:58 +0200

    cleanup circular import of models

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,6 @@
 from flask import Flask
-from app.auth import bp as auth_bp
-from app.forum import bp as forum_bp
-from flask_sqlalchemy import SQLAlchemy
 
+from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 app = Flask(__name__)
@@ -12,11 +10,11 @@ app.config.from_object("config.DevConfig")
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
+from app.auth import bp as auth_bp
+from app.forum import bp as forum_bp
+
 app.register_blueprint(auth_bp)
 app.register_blueprint(forum_bp)
 
-
 from app.auth import views
 from app.forum import views
-
-from app import models
--- app/auth/views.py
+++ app/auth/views.py
@@ -3,6 +3,7 @@ from app import app
 from flask import render_template, request, redirect, url_for
 
 from .forms import LoginForm, RegisterForm
+from app.models import User
 
 
 @app.route("/login", methods=["GET", "POST"])
@@ -10,8 +11,9 @@ def login():
     form = LoginForm(request.form)
     if form.validate_on_submit():
         print(form)
+
         # TODO
-        return redirect('/success')
+        return redirect(url_for("forum.home"))
     return render_template("auth/login.jinja", form=form)
 
 
@@ -22,5 +24,5 @@ def register():
         print(form)
 
         # TODO
-        return redirect(url_for("index"))
+        return redirect(url_for("forum.home"))
     return render_template("auth/register.jinja", form=form)
